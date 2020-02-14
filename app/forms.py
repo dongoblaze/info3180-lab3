aef9fa352a8516037624a51765f30fe38c4243b4
Omari Domville <you@example.com>
2020-02-13 21:39:58 -0500

    updated forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import Email 
 
@@ -7,5 +8,5 @@ class ContactForm(FlaskForm):
  name = StringField('Name', validators=[DataRequired()])
  email = StringField('Email', validators=[DataRequired(), Email()])
  subject = StringField('Subject', validators=[DataRequired()])
- message = StringField('Message', validators=[DataRequired(), ])
+ message = TextAreaField('Message', validators=[DataRequired(), ])
 
