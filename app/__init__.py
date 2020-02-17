2424eaad490ee2744b67557075d5012d897d2e41
Omari Domville <you@example.com>
2020-02-16 22:45:51 -0500

    deleted

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'enter some random passphrase'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525'
-app.config['MAIL_USERNAME'] = '08df8cd41b5019'
-app.config['MAIL_PASSWORD'] = 'acb045050b1316'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 mail = Mail(app)
 from app import views
